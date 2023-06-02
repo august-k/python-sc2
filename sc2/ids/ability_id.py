374105b2559084241c6030db75137e717f59e52e
Tom Kerr <tomkerrdev@gmail.com>
2023-06-02 14:20:24 +0100

    fix: prevent crash on missing abilityid

--- sc2/ids/ability_id.py
+++ sc2/ids/ability_id.py
@@ -1587,6 +1587,10 @@ class AbilityId(enum.Enum):
     def __repr__(self) -> str:
         return f"AbilityId.{self.name}"
 
+    @classmethod
+    def _missing_(cls, value: int) -> "AbilityId":
+        return cls.NULL_NULL
+
 
 for item in AbilityId:
     globals()[item.name] = item
