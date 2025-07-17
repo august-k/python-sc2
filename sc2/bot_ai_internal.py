481ab04fbd6791a6c78f331a37f0d3ae14b27fc0
rasper <tomkerrdev@gmail.com>
2025-07-17 16:39:14 +0100

    cache unit abilities for all units and structures

--- sc2/bot_ai_internal.py
+++ sc2/bot_ai_internal.py
@@ -698,7 +698,7 @@ class BotAIInternal(ABC):
         self.army_count: int = state.common.army_count
 
         self._unit_abilities = await self.client.query_available_abilities_with_tag(
-            self.units,
+            self.all_units,
             ignore_resource_requirements=False,
         )
         self._time_before_step: float = time.perf_counter()
